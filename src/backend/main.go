f90e39aa1f1eaf5b7a89a78fa54b0429fd09033d
Akshat Garg <akshatarungarg78@gmail.com>
2021-03-05 13:21:07 +0530

    created entrycard component

--- src/backend/main.go
+++ src/backend/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	_ "backend/routers"
+	"fmt"
 
+	"github.com/beego/beego/v2/client/orm"
 	beego "github.com/beego/beego/v2/server/web"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -11,5 +14,17 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	if err := orm.RegisterDriver("postgres", orm.DRPostgres); err != nil {
+		fmt.Println("register driver failed")
+	}
+	sqlConnString, _ := beego.AppConfig.String("sqlconn")
+	fmt.Println(sqlConnString)
+	if err := orm.RegisterDataBase("default", "postgres", "host=127.0.0.1 port=5432 dbname=test sslmode=disable"); err != nil {
+		fmt.Println("register database failed")
+	} else {
+		fmt.Println("database connected")
+	}
+
 	beego.Run()
 }
