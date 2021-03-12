6455bb4c714a134b941798d24b0375f341b4dd04
Akshat Garg <60404253+akshatgarg12@users.noreply.github.com>
2021-03-12 17:55:25 +0530

    router dom and admin page init (git issues resolved) (#11)
    * created navbar and made the accounts table mobile friendly
    
    * added charts for demographs and embeded them to side drawer
    
    * added routing
    
    * changing navbar for diff page
    
    * admin page init
    
    * resolving issue

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,13 +1,25 @@
 import DashBoard from './components/Pages/Dashboard'
 import Navbar from './components/templates/Navbar';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import AdminPage from './components/Pages/Admin';
+
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <DashBoard />
-    </div>
+    <Router>
+      <Switch>
+        <div className="App">
+          <Navbar />
+          <Route path="/" exact component={DashBoard} />
+          <Route path="/admin" component={AdminPage} />
+        </div>
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
--- src/frontend/src/components/templates/Navbar/index.tsx
+++ src/frontend/src/components/templates/Navbar/index.tsx
@@ -3,19 +3,37 @@ import {Button, Heading, useDisclosure} from '@chakra-ui/react'
 import React from 'react'
 import SideDrawer from "../../SideDrawer"
 
+import {Link, useLocation} from 'react-router-dom'
+
+
+
 export interface NavbarProps {
   
 }
  
 const Navbar: React.FC<NavbarProps> = () => {
+
+  const location = useLocation()
+
+  const currentPath = location.pathname
+  const navLinkPath = currentPath === "/admin" ? "/" : "/admin"
+  const navLinkName = navLinkPath === "/admin" ? "Admin" : "Dashboard"
+  const showDemographButton:boolean = currentPath === "/" 
+
   const { isOpen, onOpen, onClose } = useDisclosure()
   const firstField = React.useRef(null)
+
   return (
     <nav className="navbar">
       <Heading d="inline" size="lg">Recko</Heading>
-      <Button  colorScheme="gray" onClick={onOpen}>
-        Demographs
-      </Button>
+
+      <div className="navbar-options">
+        <h6><Link to={navLinkPath}>{navLinkName}</Link></h6>
+        {showDemographButton && <Button margin="auto 0" d="inline" colorScheme="gray" onClick={onOpen}>
+          Demographs
+        </Button>}
+      </div>
+      
       <SideDrawer 
         isOpen = {isOpen}
         onClose = {onClose}
