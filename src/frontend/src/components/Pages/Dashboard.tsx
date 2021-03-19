dc48c27538852b45b259bb15ea291fbb29a84d88
Akshat Garg <akshatarungarg78@gmail.com>
2021-03-19 14:57:49 +0530

    connected to backend with useFetch hook

--- src/frontend/src/components/Pages/Dashboard.tsx
+++ src/frontend/src/components/Pages/Dashboard.tsx
@@ -1,21 +1,41 @@
 import AccountTable from '../AccountTable'
+import useFetch from '../../hooks/useFetch'
 
 export interface DashBoardProps {
   
 }
  
 const DashBoard: React.FC<DashBoardProps> = () => {
+  const {state, loading, error} = useFetch({
+    url:"/journal",
+    headers:{
+      "Content-type":"application/json"
+    },
+    body:null,
+    method:"GET"
+  })
+  if(error) {
+    console.log(error)
+    return <h1>error</h1>
+  }
+  if(loading){
+    return <h1>Loading...</h1>
+  }
   return (
-    <>
-    <AccountTable 
-      accounts = {[
-        {id : "1", accountId:"abcd1234", accountName:"Triumph", date:"21/06/2021", provider:"XERO", type:"credit", amount:2000},
-        {id : "2", accountId:"abcd1234", accountName:"Triumph", date:"21/06/2021", provider:"XERO", type:"debit", amount:2000},
-        {id : "3", accountId:"abcd1234", accountName:"Triumph", date:"21/06/2021", provider:"XERO", type:"credit", amount:2000}
-      ]}
-    />
-    </>
-  );
+      <>
+        <AccountTable 
+          accounts = {state}
+        />
+      </>
+    );
+  
 }
  
 export default DashBoard;
+
+
+// [
+//   {id : "1", accountId:"abcd1234", accountName:"Triumph", date:"21/06/2021", provider:"XERO", type:"credit", amount:2000},
+//   {id : "2", accountId:"abcd1234", accountName:"Triumph", date:"21/06/2021", provider:"XERO", type:"debit", amount:2000},
+//   {id : "3", accountId:"abcd1234", accountName:"Triumph", date:"21/06/2021", provider:"XERO", type:"credit", amount:2000}
+// ]
