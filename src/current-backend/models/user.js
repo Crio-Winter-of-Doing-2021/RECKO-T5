00d38a22dd345bc9512c9f18aa0e0785fbc9ce72
Akshat Garg <akshatarungarg78@gmail.com>
2021-03-28 13:44:14 +0530

    user schema created

---
+++ src/current-backend/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required:[true, "username is required"]
+  },
+  email:{
+    type:String,
+    required:[true, "user email is required"],
+    unique:[true, "user email must be unique"]
+  },
+  password:{
+    type:String,
+    required:[true, "password is required"],
+    validate:[passwordValidator, "Password must be of length > 5"]
+  },
+  admin:{
+    type:Boolean,
+    required:[true, "Specify user role"],
+    default:false
+  }
+})
+
+const passwordValidator = (pass) => {
+  return pass.length > 5
+}
+
+module.exports = new mongoose.model(UserSchema, "User");
