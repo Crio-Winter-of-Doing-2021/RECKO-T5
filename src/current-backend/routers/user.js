0b3308b674f204fb0197480c3babf4b47005c292
Akshat Garg <akshatarungarg78@gmail.com>
2021-03-28 14:25:44 +0530

    user auth routes

---
+++ src/current-backend/routers/user.js
@@ -0,0 +1,8 @@
+const UserController = require('../controllers/user')
+const router = require('express').Router()
+
+router.post('/login', UserController.login)
+router.post('/register', UserController.register)
+router.post('/logout', UserController.logout)
+
+module.exports = router
